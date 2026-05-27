e22fe98693f70a721f7e3a6654521db07e7fc872
Ai Takeuchi <165442194+ge-editor@users.noreply.github.com>
2026-05-27 16:44:12 +0900

    deploysession: add tag v0.1.8 for gelog

--- functions_release.go
+++ functions_release.go
@@ -5,6 +5,8 @@
 package gelog
 
 func Info(msg string, args ...any)  {}
+func Warn(msg string, args ...any)  {}
 func Error(msg string, args ...any) {}
 func Debug(msg string, args ...any) {}
 func Key(msg string, args ...any)   {}
+func CallerInfo() string            { return "" }
