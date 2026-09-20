1ae3a8c780860dad63ce9b634de90fee6c522837
Ai Takeuchi <165442194+ge-editor@users.noreply.github.com>
2026-09-20 14:12:33 +0900

    for tag v0.1.4

--- functions_release.go
+++ functions_release.go
@@ -8,3 +8,4 @@ func Info(msg string, args ...any)  {}
 func Error(msg string, args ...any) {}
 func Debug(msg string, args ...any) {}
 func Key(msg string, args ...any)   {}
+func CallerInfo() string            { return "" }
